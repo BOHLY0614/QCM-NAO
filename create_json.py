5e8bc7af9049288bfe3da237fd6a4634a6d115d0
BOHLY0614 <nicolasbohly1@gmail.com>
2026-02-05 11:27:20 +0100

    add questions roumain

--- create_json.py
+++ create_json.py
@@ -69,9 +69,9 @@ def read_questions_and_answers(questions_filename, answers_filename):
     return questions
 
 # Partie pour radio
-questions = read_questions_and_answers("Ophta2FRQ.txt", "Ophta2FRR.txt")
+questions = read_questions_and_answers("Ophta5RQ.txt", "Ophta5RR.txt")
 
-input_file = "Ophta2FRR.txt"
+input_file = "Ophta5RR.txt"
 convert_to_uppercase_inplace(input_file)
 
 with open(input_file, "r") as f:
