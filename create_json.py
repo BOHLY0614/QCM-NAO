1516edc081fc92419effe9ad2b8f6f875ff1a74c
BOHLY0614 <nicolasbohly1@gmail.com>
2026-06-07 19:00:25 +0200

    add puéri

--- create_json.py
+++ create_json.py
@@ -69,9 +69,9 @@ def read_questions_and_answers(questions_filename, answers_filename):
     return questions
 
 # Partie pour radio
-questions = read_questions_and_answers("OncoEN2Q.txt", "OncoEN2R.txt")
+questions = read_questions_and_answers("PuériQ.txt", "PuériR.txt")
 
-input_file = "OncoEN2R.txt"
+input_file = "PuériR.txt"
 convert_to_uppercase_inplace(input_file)
 
 with open(input_file, "r") as f:
